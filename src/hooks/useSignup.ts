10db57569081f5f2dee41b09f2b7c71a2fe116b6
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-06-16 18:01:42 +0530

    Update useSignup.ts

--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -37,7 +37,7 @@ const useSignup = (): SignupResult => {
 			console.log("work is working")
 			setAuthUser(data);
 		} catch (error) {
-			toast.error(error.message);
+			toast.error("error");
 		} finally {
 			setLoading(false);
 		}
