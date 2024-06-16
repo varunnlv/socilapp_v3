048cba7bfbe5ef3af29ea8e17f644aff121b7a5c
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-06-16 19:55:51 +0530

    Update extractTime.ts

--- src/utils/extractTime.ts
+++ src/utils/extractTime.ts
@@ -1,11 +1,10 @@
-export function extractTime(dateString: string): string {
-	const date = new Date(dateString);
-	const hours = padZero(date.getHours());
-	const minutes = padZero(date.getMinutes());
-	return `${hours}:${minutes}`;
+export function extractTime(date: Date): string {
+    const hours = padZero(date.getHours());
+    const minutes = padZero(date.getMinutes());
+    return `${hours}:${minutes}`;
 }
 
 // Helper function to pad single-digit numbers with a leading zero
 function padZero(number: number): string {
-	return number.toString().padStart(2, "0");
+    return number.toString().padStart(2, "0");
 }
